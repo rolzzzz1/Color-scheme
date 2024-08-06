917eb59222ab04430f4f6842757733e44f21af03
Aishwarya Sharma <roli.mradul@gmail.com>
2024-08-06 15:50:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,28 +13,24 @@ modePicker.addEventListener("change", function () {
 });
 
 getScheme.addEventListener("click", function () {
-  //   fetch(`https://www.thecolorapi.com/scheme?hex=${colorHex}&mode=${mode}`)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       colorHex = "#" + colorHex;
-  //       document.getElementById("colorSeed").style.backgroundColor = colorHex;
-  //       document.getElementById("color1").style.backgroundColor =
-  //         data.colors[0].hex.value;
-  //       document.getElementById("color2").style.backgroundColor =
-  //         data.colors[1].hex.value;
-  //       document.getElementById("color3").style.backgroundColor =
-  //         data.colors[2].hex.value;
-  //       document.getElementById("color4").style.backgroundColor =
-  //         data.colors[3].hex.value;
-
-  //       document.getElementById("text1").textContent = colorHex;
-  //       document.getElementById("text2").textContent = data.colors[1].hex.value;
-  //       document.getElementById("text3").textContent = data.colors[2].hex.value;
-  //       document.getElementById("text4").textContent = data.colors[3].hex.value;
-  //       document.getElementById("text5").textContent = data.colors[4].hex.value;
-  //     });
-
-  fetch(`https://www.thecolorapi.com/scheme?hex=aaac2f&mode=monochrome-dark`)
+  fetch(`https://www.thecolorapi.com/scheme?hex=${colorHex}&mode=${mode}`)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      colorHex = "#" + colorHex;
+      document.getElementById("colorSeed").style.backgroundColor = colorHex;
+      document.getElementById("color1").style.backgroundColor =
+        data.colors[0].hex.value;
+      document.getElementById("color2").style.backgroundColor =
+        data.colors[1].hex.value;
+      document.getElementById("color3").style.backgroundColor =
+        data.colors[2].hex.value;
+      document.getElementById("color4").style.backgroundColor =
+        data.colors[3].hex.value;
+
+      document.getElementById("text1").textContent = colorHex;
+      document.getElementById("text2").textContent = data.colors[1].hex.value;
+      document.getElementById("text3").textContent = data.colors[2].hex.value;
+      document.getElementById("text4").textContent = data.colors[3].hex.value;
+      document.getElementById("text5").textContent = data.colors[4].hex.value;
+    });
 });
